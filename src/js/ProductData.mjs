1b90b7ac68de7ddf2c3fc6051ccb5bba8b647031
onosmister-ux <onosmister@gmail.com>
2026-07-15 18:41:04 +0100

    Implement dynamic product listing

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,15 +9,17 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `/json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    const response = await fetch(this.path);
+    return await response.json();
   }
+
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const data = await this.getData();
+
+    return data.find(product => product.Id === id);
   }
 }
