ec91be6fa6cea9d214802dc0625bfbb06663feb3
John Akala <johnolshea@gmail.com>
2026-07-15 17:20:27 +0100

    Add comment

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,7 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+//Get product id
 const productId = getParam("product");
 
 const dataSource = new ProductData("tents");
