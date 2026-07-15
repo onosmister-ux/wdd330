cbf31414168f2a7f0aa4d68d2c4e3022a6619ab6
onosmister-ux <onosmister@gmail.com>
2026-07-15 15:14:10 +0100

    Fix localStorage cart persistence

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-// cart fix
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
